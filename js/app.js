867878250b1ec25de70a2f550235494a3e7d86a6
Amir Mavaee <ah.mavaee3831@gmail.com>
2024-02-10 06:52:23 -0800

    first commit

---
+++ js/app.js
@@ -0,0 +1,49 @@
+const swiper = new Swiper('.headerSlider', {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    loop: true,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    autoplay: {
+        delay: 4000,
+        disableOnInteraction: false,
+    },
+});
+
+const specialSwiper = new Swiper('.mySpecialSaleSwiper', {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    loop: true,
+    navigation: {
+        nextEl: '.special-next',
+        prevEl: '.special-prev',
+    },
+    // autoplay: {
+    //     delay: 3000,
+    //     disableOnInteraction: false,
+    // },
+});
+
+const MostSwiper = new Swiper('.myMostSaleSwiper', {
+    slidesPerView: 1,
+    spaceBetween: 0,
+    loop: true,
+    navigation: {
+        nextEl: '.most-next',
+        prevEl: '.most-prev',
+    },
+    // autoplay: {
+    //     delay: 2000,
+    //     disableOnInteraction: false,
+    // },
+    breakpoints: {
+        768: {
+          slidesPerView: 2,
+        },
+        992: {
+          slidesPerView: 3,
+        },
+      },
+});
