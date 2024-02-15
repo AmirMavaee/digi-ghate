ed21905ed822b006473a03bc31cbbaa86fb390fb
Amir Mavaee <ah.mavaee3831@gmail.com>
2024-02-15 08:57:41 -0800

    change  cart

--- js/jQuery-app.js
+++ js/jQuery-app.js
@@ -1,19 +1,20 @@
 $(document).ready(function () {
-    $(".question-header").click(function (e) { 
-        e.preventDefault();        
-        $(this).find('.pluse-image').toggleClass('rotate-135');
-    });
+  $(".question-header").click(function (e) {
+    e.preventDefault();
+    $(this).find(".pluse-image").toggleClass("rotate-135");
+  });
 
-    let badgeText = parseInt($(".badge").text());
-    $(".add-cart").click(function () { 
-        $(".badge").removeClass("d-none");
-        badgeText+=1;
-        $(".badge").text(badgeText)
-    });
-    $(".cart").click(function () { 
-        if(badgeText === 1) 
-        $(".badge").addClass("d-none");
-        badgeText-=1;
-        $(".badge").text(badgeText)
-    });
+  let badgeText = parseInt($(".badge").text());
+  $(".add-cart").click(function () {
+    $(".badge").removeClass("d-none");
+    badgeText += 1;
+    $(".badge").text(badgeText);
+  });
+  $(".cart").click(function () {
+    if (badgeText === 1) $(".badge").addClass("d-none");
+    else {
+        badgeText -= 1;
+        $(".badge").text(badgeText);
+    }
+  });
 });
