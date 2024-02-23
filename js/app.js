238e1320e48c64a1833c63951e05f0cea927abf5
Amir Mavaee <ah.mavaee3831@gmail.com>
2024-02-23 08:10:59 -0800

    add menu responsive

--- js/app.js
+++ js/app.js
@@ -1,98 +1,98 @@
-const swiper = new Swiper('.headerSlider', {
-    slidesPerView: 1,
-    spaceBetween: 30,
-    loop: true,
-    pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-    },
-    autoplay: {
-        delay: 4000,
-        disableOnInteraction: false,
-    },
+const swiper = new Swiper(".headerSlider", {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  autoplay: {
+    delay: 4000,
+    disableOnInteraction: false,
+  },
 });
 
-const specialSwiper = new Swiper('.mySpecialSaleSwiper', {
-    slidesPerView: 1,
-    spaceBetween: 30,
-    loop: true,
-    navigation: {
-        nextEl: '.special-next',
-        prevEl: '.special-prev',
-    },
-    autoplay: {
-        delay: 3000,
-        disableOnInteraction: false,
-    },
+const specialSwiper = new Swiper(".mySpecialSaleSwiper", {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  loop: true,
+  navigation: {
+    nextEl: ".special-next",
+    prevEl: ".special-prev",
+  },
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+  },
 });
 
-const MostSwiper = new Swiper('.myMostSaleSwiper', {
-    slidesPerView: 1,
-    spaceBetween: 0,
-    loop: true,
-    navigation: {
-        nextEl: '.most-next',
-        prevEl: '.most-prev',
+const MostSwiper = new Swiper(".myMostSaleSwiper", {
+  slidesPerView: 1,
+  spaceBetween: 0,
+  loop: true,
+  navigation: {
+    nextEl: ".most-next",
+    prevEl: ".most-prev",
+  },
+  autoplay: {
+    delay: 2000,
+    disableOnInteraction: false,
+  },
+  breakpoints: {
+    768: {
+      slidesPerView: 2,
     },
-    autoplay: {
-        delay: 2000,
-        disableOnInteraction: false,
+    992: {
+      slidesPerView: 3,
     },
-    breakpoints: {
-        768: {
-          slidesPerView: 2,
-        },
-        992: {
-          slidesPerView: 3,
-        },
-      },
+  },
 });
 
-const ProducSwiper = new Swiper('.product-swiper', {
-    slidesPerView: 1,
-    spaceBetween: 0,
-    loop: true,
-    navigation: {
-        nextEl: '.product-next',
-        prevEl: '.product-prev',
+const ProducSwiper = new Swiper(".product-swiper", {
+  slidesPerView: 1,
+  spaceBetween: 0,
+  loop: true,
+  navigation: {
+    nextEl: ".product-next",
+    prevEl: ".product-prev",
+  },
+  autoplay: {
+    delay: 3200,
+    disableOnInteraction: false,
+  },
+  breakpoints: {
+    768: {
+      slidesPerView: 2,
     },
-    autoplay: {
-        delay: 3200,
-        disableOnInteraction: false,
+    1200: {
+      slidesPerView: 3,
     },
-    breakpoints: {
-        768: {
-          slidesPerView: 2,
-        },
-        1200: {
-          slidesPerView: 3,
-        },
-        1400 :{
-          slidesPerView: 6,
-        }
-      },
+    1400: {
+      slidesPerView: 6,
+    },
+  },
 });
 
-const CustomerSwiper = new Swiper('.customer-swiper', {
+const CustomerSwiper = new Swiper(".customer-swiper", {
   slidesPerView: 2,
   spaceBetween: 0,
   loop: true,
   autoplay: {
-      delay: 2500,
-      disableOnInteraction: false,
+    delay: 2500,
+    disableOnInteraction: false,
   },
   breakpoints: {
-      576:{
-        slidesPerView: 3,
-      },
-      768: {
-        slidesPerView: 4,
-      },
-      1200:{
-        slidesPerView: 5,
-      },
-      1400:{
-        slidesPerView: 6,
-      }
+    576: {
+      slidesPerView: 3,
     },
+    768: {
+      slidesPerView: 4,
+    },
+    1200: {
+      slidesPerView: 5,
+    },
+    1400: {
+      slidesPerView: 6,
+    },
+  },
 });
--- js/jQuery-app.js
+++ js/jQuery-app.js
@@ -4,10 +4,21 @@ $(document).ready(function () {
     $(this).find(".pluse-image").toggleClass("rotate-135");
   });
 
-  let badgeText = parseInt($(".badge").text());
   $(".add-cart").click(function () {
+    let badgeText = parseInt($(".badge").text());
     $(".badge").removeClass("d-none");
     badgeText += 1;
     $(".badge").text(badgeText);
   });
+
+  $(".menu-button").click(function () { 
+    $(".responsive-menu").addClass("responsive-menu-show");
+    $(".responsive-munu-item").addClass("responsive-munu-item-show");
+  });
+
+  $(".responsive-munu-item > img").click(function () { 
+    $(".responsive-menu").removeClass("responsive-menu-show");
+    $(".responsive-munu-item").removeClass("responsive-munu-item-show");
+  });
+
 });
